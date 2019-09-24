6971959042acc23fc8689f038ff69c9a6fa1201d
FudoYusei <1034510123@qq.com>
2019-09-25 00:11:28 +0800

    fix getpostphoto to detect defaultImgUrl

--- themes/Sakura/scripts/PostHelper.js
+++ themes/Sakura/scripts/PostHelper.js
@@ -1,5 +1,7 @@
 hexo.extend.helper.register('getpostphoto', function(theme, post){
-    if(!post.photos || post.photos.length == 0) {return 'https://static.2heng.xin/wp-content/uploads//2019/02/wallhaven-672007-1-1024x576.png' };
+    var defaultImgUrl = 'https://static.2heng.xin/wp-content/uploads//2019/02/wallhaven-672007-1-1024x576.png';
+
+    if(!post.photos || post.photos.length == 0 || post.photos[0] == defaultImgUrl) {return defaultImgUrl; }
     return theme.url + '/' + post.path + post.photos[0];
 });
 
